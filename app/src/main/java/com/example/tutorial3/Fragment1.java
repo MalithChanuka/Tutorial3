0afc700a1dccadc05abc6d9396d013095b6c0a10
Malith Chanuka <mchanuka4@gmail.com>
2020-03-26 19:16:14 +0530

    Tutorial3

---
+++ app/src/main/java/com/example/tutorial3/Fragment1.java
@@ -0,0 +1,20 @@
+package com.example.tutorial03;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tutorial3.R;
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_fragment1, container, false);
+    }
+}
